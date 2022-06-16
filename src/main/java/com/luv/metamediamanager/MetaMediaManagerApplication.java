262a1a7c96709bbfe4d62e2420fdd911413e9f0d
yjlmiss <yjl4022@gmail.com>
2022-06-16 17:48:00 +0800

    init

---
+++ src/main/java/com/luv/metamediamanager/MetaMediaManagerApplication.java
@@ -0,0 +1,13 @@
+package com.luv.metamediamanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaMediaManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetaMediaManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luv/metamediamanager/MetaMediaManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv.metamediamanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaMediaManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
